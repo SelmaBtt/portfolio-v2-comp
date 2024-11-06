d12388dfee6ea71a106a9e8bfaf5dfb37833e3a4
Selma Brattli <selma@brattli.nu>
2024-11-06 18:20:39 +0100

    added calender

--- src/components/task-bar/calender/Calendar.jsx
+++ src/components/task-bar/calender/Calendar.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 import styles from './Calendar.module.css';
+import { useState, useEffect } from "react";
 
 const Calendar = () => {
+    const [date] = useState(new Date())
+    const [daysInMonth, setDaysInMonth] = useState([]);
+    
+    // daysInMonth.find((day) => day === date.getDate())
+
+    // Calender
+    useEffect(() => {
+        const days = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+        setDaysInMonth(Array.from({ length: days }, (_, i) => i + 1));
+    }, [new Date().getDay()]);
+    
+    // Current weekday
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const displayWeekday = weekdays[date.getDay()];
+    
+    // Current month
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const displayMonth = months[date.getMonth()];
+    
     return(
-        <>Calendar</>
+        <div>
+            <div>
+                <span>Today: {displayWeekday} {displayMonth} {date.getDate()}</span>
+            </div>
+            <hr />
+            <div>
+                <span>{displayMonth} {date.getFullYear()}</span>
+                <div className={styles.datesWrapper}>
+                    {daysInMonth.length > 0 && daysInMonth.map((day) => (
+                        <div
+                            key={day}
+                            className={day === date.getDate() ? styles.todayWrapper : styles.dayWrapper}
+                        >
+                            {day}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
